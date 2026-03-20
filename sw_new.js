65925c03ffa72e95bb9343c2e7e98e45e3e391c1
MikeWolfcz <michalstuchlik@pm.me>
2026-03-20 11:24:01 +0100

    Add files via upload

---
+++ sw_new.js
@@ -0,0 +1,32 @@
+const CACHE = 'hry-v3';
+const FILES = [
+  './',
+  './index.html',
+  './barvy.html',
+  './cisla.html',
+  './tlapkova.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
