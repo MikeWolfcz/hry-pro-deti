9010cef7623ee55807280e30512ca83045c08ecd
Mike <michalstuchlik@pm.me>
2026-08-29 13:31:51 +0200

    Fix icon.png -> icon-192.png references, bump SW cache to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hry-v7';
+const CACHE = 'hry-v8';
 const FILES = [
   './',
   './index.html',
@@ -6,7 +6,8 @@ const FILES = [
   './cisla.html',
   './tlapkova.html',
   './manifest.json',
-  './icon.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
